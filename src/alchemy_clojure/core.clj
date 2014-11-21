bc879c0c1bc6aa714db801d8db7d0d2a68e45a8b
Dave Liepmann <dave.liepmann@gmail.com>
2014-11-21 09:23:23 +0200

    Apply Clojure style guide
    Eliminate whitespace: move end-parens to same line as statement; comma in parameter list.
    Eliminate unnecessary (let).

--- src/alchemy_clojure/core.clj
+++ src/alchemy_clojure/core.clj
@@ -2,72 +2,43 @@
   (:require [cheshire.core   :as json])
   (:require [clj-http.client :as client]))
 
-(defonce endpoints {
-  :sentiment {
-    :url  "/url/URLGetTextSentiment"
-    :text "/text/TextGetTextSentiment"
-    :html "/html/HTMLGetTextSentiment"  
-  }
-  :sentiment_targeted {
-    :url  "/url/URLGetTargetedSentiment"
-    :text "/text/TextGetTargetedSentiment"
-    :html "/html/HTMLGetTargetedSentiment"
-  }
-  :author {
-    :url  "/url/URLGetAuthor"
-    :html "/html/HTMLGetAuthor"
-  }
-  :keywords {
-    :url  "/url/URLGetRankedKeywords"
-    :text "/text/TextGetRankedKeywords"
-    :html "/html/HTMLGetRankedKeywords"
-  }
-  :concepts {
-    :url  "/url/URLGetRankedConcepts"
-    :text "/text/TextGetRankedConcepts"
-    :html "/html/HTMLGetRankedConcepts"
-  }
-  :entities {
-    :url  "/url/URLGetRankedNamedEntities"
-    :text "/text/TextGetRankedNamedEntities"
-    :html "/html/HTMLGetRankedNamedEntities"
-  }
-  :category {
-    :url  "/url/URLGetCategory"
-    :text "/text/TextGetCategory"
-    :html "/html/HTMLGetCategory"
-  }
-  :relations {
-    :url  "/url/URLGetRelations"
-    :text "/text/TextGetRelations"
-    :html "/html/HTMLGetRelations"
-  }
-  :language {
-    :url   "/url/URLGetLanguage"
-    :text  "/text/TextGetLanguage"
-    :html  "/html/HTMLGetLanguage"
-  }
-  :text {
-    :url  "/url/URLGetText"
-    :html "/html/HTMLGetText"
-  }
-  :text_raw {
-    :url  "/url/URLGetRawText"
-    :html "/html/HTMLGetRawText"
-  }
-  :title {
-    :url  "/url/URLGetTitle"
-    :html "/html/HTMLGetTitle"
-  }
-  :feeds {
-    :url  "/url/URLGetFeedLinks"
-    :html "/html/HTMLGetFeedLinks"
-  }
-  :microformats {
-    :url  "/url/URLGetMicroformatData"
-    :html "/html/HTMLGetMicroformatData"
-  }
-})
+(defonce endpoints
+  {:sentiment {:url  "/url/URLGetTextSentiment"
+               :text "/text/TextGetTextSentiment"
+               :html "/html/HTMLGetTextSentiment" }
+   :sentiment_targeted {:url  "/url/URLGetTargetedSentiment"
+                        :text "/text/TextGetTargetedSentiment"
+                        :html "/html/HTMLGetTargetedSentiment"}
+   :author {:url  "/url/URLGetAuthor"
+            :html "/html/HTMLGetAuthor"}
+   :keywords {:url  "/url/URLGetRankedKeywords"
+              :text "/text/TextGetRankedKeywords"
+              :html "/html/HTMLGetRankedKeywords"}
+   :concepts {:url  "/url/URLGetRankedConcepts"
+              :text "/text/TextGetRankedConcepts"
+              :html "/html/HTMLGetRankedConcepts"}
+   :entities {:url  "/url/URLGetRankedNamedEntities"
+              :text "/text/TextGetRankedNamedEntities"
+              :html "/html/HTMLGetRankedNamedEntities"}
+   :category {:url  "/url/URLGetCategory"
+              :text "/text/TextGetCategory"
+              :html "/html/HTMLGetCategory"}
+   :relations {:url  "/url/URLGetRelations"
+               :text "/text/TextGetRelations"
+               :html "/html/HTMLGetRelations"}
+   :language {:url   "/url/URLGetLanguage"
+              :text  "/text/TextGetLanguage"
+              :html  "/html/HTMLGetLanguage"}
+   :text {:url  "/url/URLGetText"
+          :html "/html/HTMLGetText"}
+   :text_raw {:url  "/url/URLGetRawText"
+              :html "/html/HTMLGetRawText"}
+   :title {:url  "/url/URLGetTitle"
+           :html "/html/HTMLGetTitle"}
+   :feeds {:url  "/url/URLGetFeedLinks"
+           :html "/html/HTMLGetFeedLinks"}
+   :microformats {:url  "/url/URLGetMicroformatData"
+                  :html "/html/HTMLGetMicroformatData"}})
 
 (defonce base-url "http://access.alchemyapi.com/calls")
 
@@ -85,15 +56,16 @@
       ~docstring
      [flavor# data# & [options#]]
      (let [options# (assoc options# flavor# data#) url# (flavor# (~(keyword endpointname) endpoints))]
-    (request url# options#)))
-  )
+    (request url# options#))))
 
 (defn- request
   "HTTP request wrapper"
-  [url, options]
-  (let
-    [options (assoc options :outputMode "json" :apikey *api-key*) ]
-    (json/parse-string (:body (client/post (str base-url url) {:form-params options})) true)))
+  [url options]
+  (json/parse-string (:body (client/post (str base-url url)
+                                         {:form-params (assoc options
+                                                         :outputMode "json"
+                                                         :apikey *api-key*)}))
+                     true))
 
 (defendpoint sentiment "Calculates the sentiment for text, a URL or HTML.")
 
